72c9ec97f2f0d5cd430f2b1e9c42486645de3170
Yeh176 <15915589990@163.com>
2026-09-17 11:27:15 +0800

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -7,11 +7,16 @@ public class Sum {
         System.out.print("Enter the end number:");
         int end=input.nextInt();
         int sum = 0;
+        int evenCount = 0;
         if (start <= end) {
             for (int i = start; i <= end; i++) {
                 sum = sum + i;
+                if(i % 2 == 0){
+                    evenCount++;
+                }
             }
             System.out.println("The sum from" + start + "to" + end + "is" + sum);
+            System.out.println("Total even numbers: " + evenCount);5
         }
         else {
             System.out.println("Error: start cannot be larger than end!");
